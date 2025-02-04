98b61d13d20cf5a795c297308ee2290d0090a3b4
Denis-Omingo <omingodenis7@gmail.com>
2025-02-04 09:18:02 +0300

    chore(backend): configure initial backend environment and dependencies

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express,{Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app=express();
+app.use(express.json()); //converts every request to JSON so that we don't have to do it ourselves.
+app.use(cors());
+
+app.get("/test", async(req: Request, res: Response)=>{
+  res.json({message: "Hello Denis!"})
+})
+
+app.listen(7000, ()=>{
+    console.log("Server started on localhost: 7000");
+})
