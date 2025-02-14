cb225c30d849cc4423bb1bd6d2467abe1d84f4c9
Denis-Omingo <omingodenis7@gmail.com>
2025-02-14 15:39:29 +0300

    feat(Album view Route): Create a route to get album images

--- src/routes/MyAlbumRoute.ts
+++ src/routes/MyAlbumRoute.ts
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import MyUserController from "../controllers/MyUserController";
 import MyAlbumController from "../controllers/MyAlbumController";
 
 const router =Router();
 
 router.post("/", MyAlbumController.createMyAlbum);
 router.get("/",MyAlbumController.getMyAlbums)
+router.get("/get-albums/:id",MyAlbumController.getUserAlbums)
 router.put("/:id",MyAlbumController.updateMyAlbum)
 router.get("/:id/images",MyAlbumController.getMyImages)
 
