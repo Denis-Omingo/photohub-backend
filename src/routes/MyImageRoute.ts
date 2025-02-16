5df728cd687a80ad48a58f951ee8cc097d27be02
Denis-Omingo <omingodenis7@gmail.com>
2025-02-16 19:03:34 +0300

    feat(Edit My Photo Controller): Add a controller to edit photo title

--- src/routes/MyImageRoute.ts
+++ src/routes/MyImageRoute.ts
@@ -4,6 +4,8 @@ import uploadMiddleware from "../middlewares/uploadMiddleware";
 const router =Router();
 
 router.post("/upload-image/:id", uploadMiddleware, MyImageController.uploadImagesToAlbum);
+router.get("/user-images", MyImageController.getUserImages)
+router.put("/update/:id",MyImageController.updateMyImage)
 
 
 export default router;
